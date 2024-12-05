a35a94b5027186f1c9a680ef71beedeec6566def
Georges-ai <ilyasson06@gmail.com>
2024-12-05 15:16:25 +0100

    mqtt recieve

---
+++ Config.h
@@ -0,0 +1,5 @@
+#define ADDRESS     "tcp://192.168.0.125:1883" // !! IP-address:Port-ID
+#define CLIENTID    "FRANK"
+#define TOPIC       "test/topic"
+#define QOS         1
+#define TIMEOUT     500L
---
+++ Frank.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "MQTTClient.h"
+#include "Config.h"
+
+// This function is called upon when an incoming message from mqtt is arrived
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+    char *error_in = message->payload;
+    
+    // print incoming message
+    printf( "Received message: <%s>\n", error_in );   
+    
+    // Free the message and topic
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    
+    return 1;
+}
+
+// This function is called upon when the connection to the mqtt-broker is lost
+void connlost(void *context, char *cause) {
+    printf("\nConnection lost\n");
+    printf("     cause: %s\n", cause);
+}
+
+int main() {
+   // Open MQTT client for listening
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    int rc;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+
+    // Define the correct call back functions when messages arrive
+    MQTTClient_setCallbacks(client, client, connlost, msgarrvd, NULL);
+
+    if ((rc = MQTTClient_connect(client, &conn_opts))!= MQTTCLIENT_SUCCESS) {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Subscribing to topic %s for client %s using QoS%d\n\n", TOPIC, CLIENTID, QOS);
+    MQTTClient_subscribe(client, TOPIC, QOS);
+
+    // Keep the program running to continue receiving messages
+    for(;;) {
+        ;
+    }
+
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    return rc;
+}
